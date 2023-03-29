9aaf42b6f35ca60403680b07d03387c758d2c661
alexander <alexnander@gmail.com>
2023-03-29 17:26:54 -0400

    Watermark basic function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 import tkinter.filedialog as dlg
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, UnidentifiedImageError, ImageDraw
 import datetime
 
+# global declarations
 text_widget = None
 selected_options = ("horizontal", "30 degrees", "-30 degrees")
 selected = "horizontal"
+cropping_oversize = 220
 
 
 def open_image():
@@ -14,15 +16,47 @@ def open_image():
     # Check if file is selected
     if filename == '':
         return
-    # print(f"{filename}")
-    image1 = ImageTk.PhotoImage(Image.open(filename))
-    watermark_text = text_widget.get()
+    try:
+        image = Image.open(filename)
+        watermark_text = text_widget.get()
+        draw_watermark(image, watermark_text)
+    except UnidentifiedImageError:
+        print(f"{filename} has unrecognized format.")
 
 
 def set_selected(text):
     global selected
     # print(text)
     selected = text
+
+
+def draw_watermark(image, text):
+    watermark_text = Image.new("RGBA",
+                               (image.size[0] + cropping_oversize, image.size[1] + cropping_oversize),
+                               (255, 255, 255, 0))
+    draw_ctx = ImageDraw.Draw(watermark_text)
+
+    # draw down column every 100 px to bottom of context
+    # shift right by 50 and down 50 and repeat down column
+    # repeat until draw location is outside of
+    x_offset = 10
+    y_offset = 10
+    count = 0  # number of columns drawn so far
+    while x_offset < watermark_text.size[0]:
+        while y_offset < watermark_text.size[1]:
+            draw_ctx.text((x_offset, y_offset), text, fill=(255, 255, 255, 128))
+            y_offset += 100
+        count += 1
+        y_offset = count % 2 * 50 + 10
+        x_offset += 150
+
+    crop = cropping_oversize / 2
+    watermark_text = watermark_text.crop((crop, crop,
+                                          watermark_text.size[0] - crop, watermark_text.size[1] - crop))
+    out = Image.alpha_composite(image.convert("RGBA"), watermark_text)
+    out.show()
+
+    return
 
 
 # Setup TK window and widgets
